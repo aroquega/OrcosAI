b03ad50f2a5b4124f4a726f9db9fe07c5f412023
Hristo Roque <aroquega@unsa.edu.pe>
2020-09-29 00:27:38 -0500

    First commit

---
+++ Game/Assets/Scripts/Orc.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orc : MonoBehaviour
+{
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Move()
+    {
+    }
+}
---
+++ Game/Assets/Scripts/SelectionController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionController : MonoBehaviour
+{
+    public List<Orc> selected;
+    ParticleSystem particleSystem;
+    void Start()
+    {
+        //particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            foreach (Orc orc in selected)
+            {
+
+                Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+                RaycastHit hitData;
+
+                if (Physics.Raycast(ray, out hitData, 20))
+                {
+                    Debug.Log(hitData.point);
+                    orc.transform.position = hitData.point;
+                }
+            }
+        }
+    }
+}
