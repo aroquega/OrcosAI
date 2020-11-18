cd1bb67127f8e855d8e7be03dd5102805af0559c
ccahui <kccahui@unsa.edu.pe>
2020-11-17 22:19:28 -0500

    Seleccion y movimiento de unidades, prefabs de enemigos, escenarios de prueba

---
+++ Game/Assets/Scripts/navmesh/Zombie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Zombie : MonoBehaviour
+{
+    public Transform victim;
+    public NavMeshAgent navComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navComponent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (victim)
+        {
+            navComponent.SetDestination(victim.position);
+        }
+    }
+}
